463e63f308fe51dca7691c4d616e985c6fbdb951
Luka Papic EE42-2020 <lukapapic215@gmail.com>
2024-07-24 19:59:53 +0200

    U hardveru ostavljena isključivo samo gaussian_blur funkcija. Sve ostalo je ipak teško za realizaciju.

--- esl/bit_analysis/hard.hpp
+++ esl/bit_analysis/hard.hpp
@@ -10,26 +10,7 @@
 using namespace std;
 using namespace sc_dt;
 
-    //output structure
-    struct ScaleSpacePyramid {
-        uint8_t num_octaves;
-        uint8_t imgs_per_octave;
-        
-        std::vector<Image> images; 
-    };
-
-    //constants
-    const k_t SIGMA_MIN = 0.8;
-    const k_t MIN_PIX_DIST = 0.5;
-    const uint8_t N_OCT = 8; //Broj oktava
-    const uint8_t N_SPO = 3;
-   
     
-    ScaleSpacePyramid generate_gaussian_pyramid(const Image& img, k_t sigma_min=SIGMA_MIN,
-                                             uint8_t num_octaves=N_OCT, uint8_t scales_per_octave=N_SPO);
-                                             
-    Image resize(const Image& orig, uint16_t new_w, uint16_t new_h, Interpolation method = BILINEAR);
-
     Image gaussian_blur(const Image& img, sigma_prev_total_t sigma);
 
 #endif
