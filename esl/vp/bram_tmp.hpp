1dcf2e7c584703fd8c3615e489f11cc3b4867f36
Luka Papic EE42-2020 <lukapapic215@gmail.com>
2024-08-12 18:11:42 +0200

    Dodati bramovi za slike

---
+++ esl/vp/bram_tmp.hpp
@@ -0,0 +1,25 @@
+#ifndef _BRAM_HPP_
+#define _BRAM_HPP_
+
+#include <systemc>
+#include <tlm>
+#include <tlm_utils/simple_target_socket.h>
+#include <vector>
+#include "addr.hpp"
+#include <iostream>
+
+
+class Tmp_Bram : public sc_core::sc_module
+{
+	public:
+		Tmp_Bram (sc_core::sc_module_name name);
+		~Tmp_Bram();
+		tlm_utils::simple_target_socket<Tmp_Bram> bram_socket;
+		
+	protected:
+		void b_transport(pl_t &, sc_core::sc_time &);
+		std::vector<unsigned char> mem;
+};
+
+#endif
+
