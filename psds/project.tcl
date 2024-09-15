f23379f0da8f9762a20b84ae118fc0d65bc72e56
Luka Papic EE42-2020 <lukapapic215@gmail.com>
2024-09-15 09:39:40 +0200

    Dodata tcl skripta za generisanje projekta i pokretanje sinteze i implementacije

---
+++ psds/project.tcl
@@ -0,0 +1,47 @@
+set PROJECT_NAME              gaussian_blur_rtl
+set PROJECT_CONSTRAINT_FILE ./constraints/Zybo-Z7-Master.xdc
+
+set DIR_OUTPUT project
+            
+file mkdir ${DIR_OUTPUT}
+
+create_project ${PROJECT_NAME} ${DIR_OUTPUT}/${PROJECT_NAME} -part xc7z010clg400-1 -force
+
+add_files {rtl/dsp_unit_add.vhd}
+add_files {rtl/dsp_unit_mac_shift.vhd}
+add_files {rtl/utils_pkg.vhd}
+add_files {rtl/kernel_rom.vhd}
+add_files {rtl/convolute_loops.vhd}
+add_files {rtl/top.vhd}
+
+import_files -force
+
+import_files -fileset constrs_1 -force -norecurse ${PROJECT_CONSTRAINT_FILE}
+
+# Mimic GUI behavior of automatically setting top and file compile order
+
+update_compile_order -fileset sources_1
+
+# Launch Synthesis and wait on completion
+set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
+launch_runs synth_1
+wait_on_run synth_1
+open_run synth_1 -name netlist_1
+
+# Generate a timing and power reports and write to disk
+report_timing_summary -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file ${DIR_OUTPUT}/syn_timing.rpt
+report_power -file ${DIR_OUTPUT}/syn_power.rpt
+
+# Launch Implementation
+launch_runs impl_1 #-to_step write_bitstream
+wait_on_run impl_1 
+
+# Generate a timing and power reports and write to disk
+# comment out the open_run for batch mode
+open_run impl_1
+report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file ${DIR_OUTPUT}/imp_timing.rpt
+report_power -file ${DIR_OUTPUT}/imp_power.rpt
+
+
+# comment out the for batch mode
+start_gui
