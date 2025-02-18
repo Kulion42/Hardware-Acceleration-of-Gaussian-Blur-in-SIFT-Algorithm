3165674bee01ef4cbd0221d0f1e5d6f8f939e878
Borislav.Surovi.EE1.2020 <borislav.surovi07@gmail.com>
2025-02-18 21:17:59 +0100

    Izmenio pogresan test

--- fvh/scripts/run.tcl
+++ fvh/scripts/run.tcl
@@ -51,7 +51,7 @@ set_property -name {xsim.elaborate.xelab.more_options} -value {-v 1} -objects [g
 
 set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
 set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
-set_property -name {xsim.simulate.xsim.more_options} -value {-testplusarg UVM_TESTNAME=test_hough_simple -testplusarg UVM_VERBOSITY=UVM_LOW} -objects [get_filesets sim_1]
+set_property -name {xsim.simulate.xsim.more_options} -value {-testplusarg UVM_TESTNAME=simple_test -testplusarg UVM_VERBOSITY=UVM_LOW} -objects [get_filesets sim_1]
 
 #Pokretanje simulacije
 
